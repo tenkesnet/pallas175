f65441ad140406065af16c009d37d20a4ccfb4dc
tenkesnet <rbardo@gmail.com>
2023-10-10 19:42:25 +0200

    Dolgozz velel

--- hazi/mitru_bettina/Kor/src/pallas/Kor.java
+++ hazi/mitru_bettina/Kor/src/pallas/Kor.java
@@ -1,21 +1,26 @@
 package pallas;
-
 import java.math.*;
 import java.util.Scanner;
 
 public class Kor {
 
-	public static void main(String[] args) {
-
-		int atmero = 10;
-		int radiusz = 8;
-		int sugar = 1;
+		
+		public Kor(){
+	
+		int atmero=10;
+		int radiusz=8;
+		int sugar=1;
 		double pi = 3.14;
-
+	
+		
 		System.out.println("A kör sugara:" + sugar);
-		System.out.println("Egy egységnyi sugarú kör területe " + sugar * sugar * pi);
-		System.out.println("Egy egységnyi sugarú kör kerülete " + (sugar + sugar) * pi);
-
-	}
-
+		System.out.println("Egy egységnyi sugarú kör területe " + sugar*sugar*pi);
+		System.out.println("Egy egységnyi sugarú kör kerülete " + (sugar+sugar)*pi); 
+		
+	 }
+	 
+	
 }
+	
+
+
