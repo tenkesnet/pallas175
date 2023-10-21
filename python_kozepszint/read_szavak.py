ccbe41581ff826618de75e9a9cc4d46106f27671
vboxuser <rbardo@gmail.com>
2023-10-21 20:43:18 +0200

    Magyar szavak kieg

--- python_kozepszint/read_szavak.py
+++ python_kozepszint/read_szavak.py
@@ -1,10 +1,32 @@
-word_list = []
-for line in open("data/magyar-szavak.txt","r", encoding="utf-8"):
-    word = line.strip()
-    word_list.append(word)
+import time
+from typing import List
 
-print(len(word_list))
+class SzotarElem:
+    def __init__(self,szo,lemma,szofaj,szotagszam,cv,szoGyak,lemmaGyak):
+        self.szo=szo
+        self.lemma=lemma
+        self.szofaj=szofaj
+        self.szotagszam=szotagszam
+        self.cv=cv
+        self.szGyak = szoGyak
+        self.lemmaGyak = lemmaGyak
+
+
+def loadSzavak():
+    word_list :List[SzotarElem] = []
+    for line in open("data/magyar-szavak-kat.txt","r", encoding="utf-8"):
+        word = line.strip().split("\t")
+        if(len(word)<8):
+            break
+        e = SzotarElem(word[0],word[1],word[2],word[4],word[5],word[6],word[7])
+        word_list.append(e)
+    return word_list
+
 
 wlist =  open("data/magyar-szavak.txt","r", encoding="utf-8").read()
 
-print(len(wlist))
+start = time.time()
+print(len(loadSzavak()))
+end = time.time()
+print(f"Futási idő: {round((end-start)*1000)}")
+
